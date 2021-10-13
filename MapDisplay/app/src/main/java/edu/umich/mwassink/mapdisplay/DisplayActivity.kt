4de9919de9f0ae1bce4209a7698b592501add6e6
mwassink <mwassink@umich.edu>
2021-10-13 19:44:13 -0400

    gl setup working

--- MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/DisplayActivity.kt
+++ MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/DisplayActivity.kt
@@ -1,9 +1,21 @@
 package edu.umich.mwassink.mapdisplay
 
 import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 
-class DisplayActivity : Activity {
+class DisplayActivity: Activity() {
 
+    lateinit var view: DisplayView
+    init {
+
+    }
+
+    override fun onCreate(bundle: Bundle?): Unit{
+        super.onCreate(bundle)
+        view = DisplayView(this)
+        setContentView(view)
+    }
 
 }
--- MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/MainActivity.kt
+++ MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.umich.mwassink.mapdisplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun launchGL(v: View) {
+        startActivity(Intent(this, DisplayActivity::class.java))
     }
 }
