d45bd09bb02b3495cefa5863bd21a39631dfebfa
MarcoCL <comandma@umich.edu>
2021-10-26 16:55:42 -0400

    Created ReturnButton for Arrival message

--- MGuide/app/src/main/java/edu/umich/wwf/MainActivity.kt
+++ MGuide/app/src/main/java/edu/umich/wwf/MainActivity.kt
@@ -1,11 +1,13 @@
 package edu.umich.wwf
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -55,17 +57,25 @@ class MainActivity : AppCompatActivity() {
         arrive_button_view.setOnClickListener {
             val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val view = inflater.inflate(R.layout.arrival, null)
+
             val popupWindow = PopupWindow(
                 view,
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
+
             popupWindow.showAtLocation(
                 arrive_button_view, // Location to display popup window
                 Gravity.CENTER, // Exact position of layout to display popup
                 0, // X offset
                 0 // Y offset
             )
+
+            val returnButton = view.findViewById<Button>(R.id.returnButton)
+            returnButton.setOnClickListener{
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
 
 
