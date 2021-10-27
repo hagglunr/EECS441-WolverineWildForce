4c76d496104816728826b121900a62dceb41590e
MarcoCL <comandma@umich.edu>
2021-10-27 15:15:34 -0400

    Refactored code for Search
    Created one single view var and accesses different views from it, instead of calling findViewById

--- MGuide/app/src/main/java/edu/umich/wwf/MainActivity.kt
+++ MGuide/app/src/main/java/edu/umich/wwf/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.umich.wwf
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
@@ -9,75 +10,71 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import edu.umich.wwf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var building_spinner_adapter: ArrayAdapter<String>
-    private lateinit var room_spinner_adapter: ArrayAdapter<String>
+    private lateinit var view: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val building_spinner_view = findViewById<Spinner>(R.id.building_spinner)
-        building_spinner_adapter = ArrayAdapter<String>(this,
-                                                        android.R.layout.simple_spinner_item,
-                                                        resources.getStringArray(R.array.buildings))
-        building_spinner_view.adapter = building_spinner_adapter
+        view = ActivityMainBinding.inflate(layoutInflater)
+        view.root.setBackgroundColor(
+            Color.parseColor("#3399FF"))
+        setContentView(view.root)
 
-        val room_spinner_view = findViewById<Spinner>(R.id.room_spinner)
-        room_spinner_adapter = ArrayAdapter(this,
+        view.buildingSpinnerView.setAdapter(ArrayAdapter<String>(this,
                                             android.R.layout.simple_spinner_item,
-                                            arrayOf<String>())
-        room_spinner_view.adapter = room_spinner_adapter
+                                            resources.getStringArray(R.array.buildings)))
 
-        building_spinner_view.onItemSelectedListener = object :  AdapterView.OnItemSelectedListener {
+        view.roomSpinnerView.setAdapter(ArrayAdapter(this,
+                                        android.R.layout.simple_spinner_item,
+                                        arrayOf<String>()))
+
+        view.buildingSpinnerView.onItemSelectedListener = object :  AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 val building = resources.getStringArray(R.array.buildings)[p2]
                 val building_id = resources.getIdentifier(building, "array", this@MainActivity.getPackageName())
                 Toast.makeText(applicationContext, "Building $building selected", Toast.LENGTH_SHORT).show()
-                room_spinner_view.adapter = ArrayAdapter(this@MainActivity,
-                                                     android.R.layout.simple_spinner_item,
-                                                     resources.getStringArray(building_id))
+                view.roomSpinnerView.setAdapter(ArrayAdapter(this@MainActivity,
+                                                android.R.layout.simple_spinner_item,
+                                                resources.getStringArray(building_id)))
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
 
-        val search_button_view = findViewById<Button>(R.id.search_button)
-        search_button_view.setOnClickListener {
-            val building = building_spinner_view.getSelectedItem().toString()
-            val room = room_spinner_view.getSelectedItem().toString()
+        view.searchButton.setOnClickListener {
+            val building = view.buildingSpinnerView.getSelectedItem().toString()
+            val room = view.roomSpinnerView.getSelectedItem().toString()
             Toast.makeText(applicationContext, "Searching $building $room ...", Toast.LENGTH_SHORT).show()
         }
 
-        // button for demonstrating the arrival message popup window
-        val arrive_button_view = findViewById<Button>(R.id.arrive_button)
-        arrive_button_view.setOnClickListener {
+        view.arriveButton.setOnClickListener {
             val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val view = inflater.inflate(R.layout.arrival, null)
+            val popupView = inflater.inflate(R.layout.arrival, null)
 
             val popupWindow = PopupWindow(
-                view,
+                popupView,
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
 
             popupWindow.showAtLocation(
-                arrive_button_view, // Location to display popup window
+                view.arriveButton, // Location to display popup window
                 Gravity.CENTER, // Exact position of layout to display popup
                 0, // X offset
                 0 // Y offset
             )
 
-            val returnButton = view.findViewById<Button>(R.id.returnButton)
+            val returnButton = popupView.findViewById<Button>(R.id.returnButton)
             returnButton.setOnClickListener{
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
         }
-
 
     }
 }
