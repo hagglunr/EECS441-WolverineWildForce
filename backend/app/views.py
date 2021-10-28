f6291ab5f49e09892a4cfd9b3f9cc6c2e711fc89
Ubuntu <ubuntu@ip-172-31-38-142.us-east-2.compute.internal>
2021-10-28 01:05:15 +0000

    started views

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
+
+def getNodes(request):
+    if request.method != 'GET':
+	return HttpResponse(status=404)
+    response = {}
+    if !request.GET.get('building', '').building:
+	return  HttpResponse(status=404)
+    
