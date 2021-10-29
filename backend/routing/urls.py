5d9c40a9a01083462221b6e6fefddac71b05a1a6
Ubuntu <ubuntu@ip-172-31-38-142.us-east-2.compute.internal>
2021-10-29 21:44:30 +0000

    backend routing added

--- backend/routing/urls.py
+++ backend/routing/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
+    path('getNodes/', views.getNodes, name='getNodes'),
+    path('postNodes/', views.postNodes, name='postNodes'),
     path('admin/', admin.site.urls),
 ]
