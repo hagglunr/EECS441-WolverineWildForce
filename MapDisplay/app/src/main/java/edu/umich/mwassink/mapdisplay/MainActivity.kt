71d695f47f603dc13eb0f639dc001d0917cf35a4
MarcoCL <comandma@umich.edu>
2021-11-22 17:36:07 -0500

    Added Code to Handle the Transition between Different States in NavigationActivity

--- MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/MainActivity.kt
+++ MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 val building = resources.getStringArray(R.array.buildings)[p2]
                 val building_id = resources.getIdentifier(building, "array", this@MainActivity.getPackageName())
-                Toast.makeText(applicationContext, "Building $building selected", Toast.LENGTH_SHORT).show()
+                // Toast.makeText(applicationContext, "Building $building selected", Toast.LENGTH_SHORT).show()
                 view.roomSpinnerView.setAdapter(ArrayAdapter(this@MainActivity,
                                                 android.R.layout.simple_spinner_item,
                                                 resources.getStringArray(building_id)))
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(Intent(this, DebugActivity::class.java))
             val building = view.buildingSpinnerView.getSelectedItem().toString()
             val room = view.roomSpinnerView.getSelectedItem().toString()
-            Toast.makeText(applicationContext, "Searching $building $room ...", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(applicationContext, "Searching $building $room ...", Toast.LENGTH_SHORT).show()
             val i = Intent(this, NavigationActivity::class.java)
             i.putExtra("building", building)
             i.putExtra("room", room)
--- MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/NavigationActivity.kt
+++ MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/NavigationActivity.kt
@@ -1,15 +1,71 @@
 package edu.umich.mwassink.mapdisplay
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import edu.umich.mwassink.mapdisplay.NavigationActivity.NavigationState.*
 import edu.umich.mwassink.mapdisplay.databinding.ActivityMainBinding
 import edu.umich.mwassink.mapdisplay.databinding.ActivityNavigationBinding
 
 class NavigationActivity : AppCompatActivity() {
 
+    enum class NavigationState {
+        BROWSING, NAVIGATING, REPONSITIONING_B, REPONSITIONING_N, ARRIVE
+    }
+
     private lateinit var view: ActivityNavigationBinding
+    private var currState: NavigationState = BROWSING
+
+    private fun showStart() {
+        view.startButton.visibility = Button.VISIBLE
+    }
+    private fun hideStart() {
+        view.startButton.visibility = Button.INVISIBLE
+    }
+    private fun showReposition() {
+        view.repositionButton.text = "Reposition"
+        view.repositionButton.setBackgroundColor(Color.parseColor("#FBC520"))
+    }
+    private fun showRepositionDone() {
+        view.repositionButton.text = "Done"
+        view.repositionButton.setBackgroundColor(Color.parseColor("#6EE14F"))
+    }
+    private fun transition(clickedButton: View) {
+        with (clickedButton as Button) {
+            Log.d("Transition", "CurrState: $currState, Action: $text, $id")
+        }
+        when (clickedButton.id) {
+            view.startButton.id -> {
+                when (currState) {
+                    BROWSING -> { currState = NAVIGATING; hideStart() }
+                }
+            }
+            view.repositionButton.id -> {
+                with (clickedButton as Button) {
+                    if (text == "Reposition") {
+                        when (currState) {
+                            BROWSING -> { currState = REPONSITIONING_B }
+                            NAVIGATING -> { currState = REPONSITIONING_N }
+                        }
+                        hideStart()
+                        showRepositionDone()
+                    }
+                    else {
+                        when (currState) {
+                            REPONSITIONING_B -> { currState = BROWSING; showStart() }
+                            REPONSITIONING_N -> { currState = NAVIGATING }
+                        }
+                        showReposition()
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +75,21 @@ class NavigationActivity : AppCompatActivity() {
         val building = getIntent().getExtras()?.getString("building")
         val room = getIntent().getExtras()?.getString("room")
 
-        Toast.makeText(applicationContext, "navigationActivity received: ${building + room}" , Toast.LENGTH_SHORT).show()
         view.destination.text = "$building $room"
+
+        view.startButton.setOnClickListener{
+            transition(it)
+        }
+
+        view.repositionButton.setOnClickListener{
+            transition(it)
+        }
 
         view.exitButton.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
 
 }
