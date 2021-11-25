6359f986234a2e2e4b9e89393a5b9a8efbbf1056
lisalcao <46034325+lisalcao@users.noreply.github.com>
2021-11-25 14:15:03 -0500

    pop up message for reposition & gesture image

--- MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/NavigationActivity.kt
+++ MapDisplay/app/src/main/java/edu/umich/mwassink/mapdisplay/NavigationActivity.kt
@@ -1,7 +1,13 @@
 package edu.umich.mwassink.mapdisplay
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.umich.mwassink.mapdisplay.databinding.ActivityMainBinding
@@ -25,6 +31,30 @@ class NavigationActivity : AppCompatActivity() {
         view.exitButton.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        view.repositionButton.setOnClickListener {
+            val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.reposition_tutorial, null)
+
+            val popupWindow = PopupWindow(
+                popupView,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            popupWindow.showAtLocation(
+                view.repositionButton, // Location to display popup window
+                Gravity.CENTER, // Exact position of layout to display popup
+                0, // X offset
+                0 // Y offset
+            )
+
+            val reposTutorialOKbutton = popupView.findViewById<Button>(R.id.reposTutorialOKbutton)
+            reposTutorialOKbutton.setOnClickListener{
+                val intent = Intent(this, NavigationActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
